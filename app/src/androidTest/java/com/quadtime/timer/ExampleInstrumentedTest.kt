56404bd36868277db17dc6ff6ee9efd1583080a0
benrpinto <benrpinto@hotmail.com>
2023-06-03 15:53:03 +1000

    Added some instrumented tests for play pause button and timeout button

--- app/src/androidTest/java/com/quadtime/timer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/quadtime/timer/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.quadtime.timer
 
+import android.content.Intent
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.not
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,70 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Before
+    fun init() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(appContext,MainActivity::class.java).apply{
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        appContext.startActivity(intent)
+    }
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.timer", appContext.packageName)
+        assertEquals("com.quadtime.timer", appContext.packageName)
     }
+
+    @Test
+    fun playPause(){
+        Espresso.onView(withId(R.id.playPauseButton))
+            .check(ViewAssertions.matches(ViewMatchers.withContentDescription(R.string.play_button)))
+        Espresso.onView(withId(R.id.playPauseButton))
+            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.playPauseButton))
+            .check(ViewAssertions.matches(ViewMatchers.withContentDescription(R.string.pause_button)))
+        Espresso.onView(withId(R.id.playPauseButton))
+            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.playPauseButton))
+            .check(ViewAssertions.matches(ViewMatchers.withContentDescription(R.string.play_button)))
+    }
+
+    @Test
+    fun timeout(){
+
+        Espresso.onView(withId(R.id.timeout))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.timeout)))
+
+        //check that the timeoutCounter and associated buttons and views are not visible
+        Espresso.onView(withId(R.id.timeoutRow))
+            .check(ViewAssertions.matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.timeoutCounter))
+            .check(ViewAssertions.matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.minus1))
+            .check(ViewAssertions.matches(not(isDisplayed())))
+        Espresso.onView(withId(R.id.plus1))
+            .check(ViewAssertions.matches(not(isDisplayed())))
+
+        //click Timeout button
+        Espresso.onView(withId(R.id.timeout))
+            .perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.timeout))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.clear_timeout)))
+
+        //check that the timeoutCounter and associated buttons are visible
+        Espresso.onView(withId(R.id.timeoutRow))
+            .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(withId(R.id.timeoutCounter))
+            .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(withId(R.id.minus1))
+            .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(withId(R.id.plus1))
+            .check(ViewAssertions.matches(isDisplayed()))
+
+    }
+
 }
